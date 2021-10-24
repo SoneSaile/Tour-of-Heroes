b26f76420aaf338357109f1ac861474a998d8260
Enos Elias Barra Nova Monteiro <enoselias28@gmail.com>
2021-10-24 15:23:04 -0300

    Heroes details implementation

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 1, name: 'Axe', url: 'https://static.wikia.nocookie.net/dota2_gamepedia/images/2/23/Axe_icon.png'},
+    { id: 2, name: 'Bristleback', url: 'https://static.wikia.nocookie.net/dota2_gamepedia/images/4/4d/Bristleback_icon.png'},
+    { id: 3, name: 'Clinkz', url: 'https://static.wikia.nocookie.net/dota2_gamepedia/images/c/cb/Clinkz_icon.png'},
+    { id: 4, name: 'Disruptor', url: 'https://static.wikia.nocookie.net/dota2_gamepedia/images/9/97/Disruptor_icon.png'},
+    { id: 5, name: 'Earth Spirit', url: 'https://static.wikia.nocookie.net/dota2_gamepedia/images/b/be/Earth_Spirit_icon.png'},
+    { id: 6, name: 'Faceless Void', url: 'https://static.wikia.nocookie.net/dota2_gamepedia/images/7/73/Faceless_Void_icon.png'},
+    { id: 7, name: 'Grimstroke', url: 'https://static.wikia.nocookie.net/dota2_gamepedia/images/d/d7/Grimstroke_icon.png'},
+    { id: 8, name: 'Huskar', url: 'https://static.wikia.nocookie.net/dota2_gamepedia/images/d/d3/Huskar_icon.png'},
+    { id: 9, name: 'Invoker', url: 'https://static.wikia.nocookie.net/dota2_gamepedia/images/0/00/Invoker_icon.png'},
+    { id: 10, name: 'Juggernaut', url: 'https://static.wikia.nocookie.net/dota2_gamepedia/images/0/03/Juggernaut_icon.png'},
+]
