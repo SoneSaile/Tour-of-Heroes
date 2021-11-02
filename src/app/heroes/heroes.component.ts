71d204d3161339ad15ad71ae47c6defb986caf9d
Enos Elias Barra Nova Monteiro <enoselias28@gmail.com>
2021-11-02 18:56:45 -0300

    Pipe async Implementation

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../Model/hero.model';
 import { HeroService } from '../services/hero.service';
-import { MessageService } from '../services/message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -10,33 +9,11 @@ import { MessageService } from '../services/message.service';
 })
 export class HeroesComponent implements OnInit {
 
-  heroes: any;
+  heroes$: any;
   erro : any;
-  constructor(private heroService: HeroService, private messageService: MessageService) {
-    this.getHeroes();
-  }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit(): void {
-  }
-
-  getHeroes() {
-    this.heroService.getHeroes().subscribe(
-      (data : Hero) =>{
-        this.heroes = data;
-        console.log("O data que recebemos", data);
-        console.log("Variável preenchida", this.heroes);
-        this.heroes.forEach((hero: any) => {
-          hero.img = "/assets/heroesImages/1.png";
-        });
-      }, 
-      (error: any) => {
-        this.erro = error;
-        console.error('ERROR: ', error);
-      }
-    );
-  }
-
-  getHeroesImages() {
-    
+    this.heroes$ = this.heroService.getHeroes();
   }
 }
